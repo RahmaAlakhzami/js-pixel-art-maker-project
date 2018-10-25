e41521f5d482001edbbe87109b2112344d81c111
RahmaAlakhzami <44445933+RahmaAlakhzami@users.noreply.github.com>
2018-10-25 19:10:54 +0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -2,33 +2,33 @@
 var height, width, color;
 
 // When size is submitted by the user, call makeGrid()
-$('#sizePicker').submit(function(event) {
-    event.preventDefault();
-    height = $('#inputHeight').val();
-    width = $('#inputWidth').val();
-    makeGrid(height, width);
-})
+$('#sizePicker').submit(function (event) {
+	event.preventDefault();
+	height = $('#inputHeight').val();
+	width = $('#inputWidth').val();
+	makeGrid(height, width);
+});
 
 
 function makeGrid(w, h) {
-    $('tr').remove();
+	$('tr').remove();
 
-    // To draw the table based on the given width and height
-    for (var row = 1; row <= w; row++) {
-        $("#pixelCanvas").append('<tr id= table' + row + '></tr>');
-        for (var col = 1; col <= h; col++) {
-            $('#table' + row).append('<td></td>');
-        }
-    }
+	// To draw the table based on the given width and height
+	for (var row = 1; row <= w; row++) {
+		$("#pixelCanvas").append('<tr id= table' + row + '></tr>');
+		for (var col = 1; col <= h; col++) {
+			$('#table' + row).append('<td></td>');
+		}
+	}
 
-    // To color the clicked square
-    $('td').click(function addColor() {
-        color = $('#colorPicker').val();
+	// To color the clicked square
+	$('td').click(function addColor() {
+		color = $('#colorPicker').val();
 
-        if ($(this).attr('style')) {
-            $(this).removeAttr('style')
-        } else {
-            $(this).attr('style', 'background-color:' + color);
-        }
-    })
+		if ($(this).attr('style')) {
+			$(this).removeAttr('style');
+		} else {
+			$(this).attr('style', 'background-color:' + color);
+		}
+	});
 }
