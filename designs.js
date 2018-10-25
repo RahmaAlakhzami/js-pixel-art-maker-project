86235ac6792cb9738ee9d32df5037f8ba1ad6628
RahmaAlakhzami <44445933+RahmaAlakhzami@users.noreply.github.com>
2018-10-25 15:53:46 +0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,27 +1,28 @@
+// variables to store the input values
 var height, width, color;
 
 // When size is submitted by the user, call makeGrid()
-    $('#sizePicker').submit(function (event){
-        event.preventDefault();
-        height = $('#inputHeight').val();
-        width = $('#inputWidth').val();
-        makeGrid(height, width);
-    })
+$('#sizePicker').submit(function(event) {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWidth').val();
+    makeGrid(height, width);
+})
 
 
-function makeGrid(w ,h) {
-        $('tr').remove();
+function makeGrid(w, h) {
+    $('tr').remove();
 
- // Your code goes here!
-    for (var row =1; row <= w; row++){
+    // To draw the table based on the given width and height
+    for (var row = 1; row <= w; row++) {
         $("#pixelCanvas").append('<tr id= table' + row + '></tr>');
-        for (var col = 1; col <= h; col++){
+        for (var col = 1; col <= h; col++) {
             $('#table' + row).append('<td></td>');
         }
     }
 
- // Add color
-    $('td').click(function addColor(){
+    // To color the clicked square
+    $('td').click(function addColor() {
         color = $('#colorPicker').val();
 
         if ($(this).attr('style')) {
